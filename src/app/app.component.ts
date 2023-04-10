15eb31603ade134e7da3dd1423726d722fa19e94
Cao Sy Hai long <caosyhailong99@gmail.com>
2023-04-10 21:41:40 +0700

    Update Angular core to v14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { UntypedFormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
@@ -9,7 +9,7 @@ import { Router } from '@angular/router';
 export class AppComponent {
   title = 'my-anime-info-website';
   selectedIndex = 0;
-  searchName = new FormControl('');
+  searchName = new UntypedFormControl('');
   constructor(
     private router: Router,
   ) { }
