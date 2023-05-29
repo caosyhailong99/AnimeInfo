fcd552ab189a45f3161a91458b74a7a6a91b7d45
Cao Sy Hai long <caosyhailong99@gmail.com>
2023-05-29 08:31:04 +0700

    format code and clear some console.log()

--- src/app/feature/searched-anime/searched-anime.component.ts
+++ src/app/feature/searched-anime/searched-anime.component.ts
@@ -11,7 +11,7 @@ import { AnimeInfoService } from 'src/app/core/services/anime-info.service';
 @Component({
   selector: 'app-searched-anime',
   templateUrl: './searched-anime.component.html',
-  styleUrls: ['./searched-anime.component.scss']
+  styleUrls: ['./searched-anime.component.scss'],
 })
 export class SearchedAnimeScreenComponent implements OnInit {
   searchedAnimeList: AnimeInfo[] = [];
@@ -34,36 +34,55 @@ export class SearchedAnimeScreenComponent implements OnInit {
   }
 
   initSearchedAnimeList() {
-    const searchedName = this.route.snapshot.queryParamMap.get("name");
+    const searchedName = this.route.snapshot.queryParamMap.get('name');
     let animeList: AnimeInfo[] = [];
     this.animeInfoService.getSearchedAnime(searchedName!).subscribe({
       next: (data: any) => {
         this.numberOfResults = data.pagination.items.total;
-        for(let item of data.data) {
+        for (let item of data.data) {
           let image = item.images;
-          let jpg_url: AnimeImageUrl = new AnimeImageUrl(image.jpg.image_url as string, image.jpg.large_image_url as string, image.jpg.small_image_url as string);
-          let webp_url: AnimeImageUrl = new AnimeImageUrl(image.webp.image_url as string, image.webp.large_image_url as string, image.webp.small_image_url as string);
+          let jpg_url: AnimeImageUrl = new AnimeImageUrl(
+            image.jpg.image_url as string,
+            image.jpg.large_image_url as string,
+            image.jpg.small_image_url as string
+          );
+          let webp_url: AnimeImageUrl = new AnimeImageUrl(
+            image.webp.image_url as string,
+            image.webp.large_image_url as string,
+            image.webp.small_image_url as string
+          );
           let animeImage: AnimeImage = new AnimeImage(jpg_url, webp_url);
           let genres = item.genres;
           let animeGenres: AnimeGenre[] = [];
-          for(let genre_item of genres) {
-            animeGenres.push(new AnimeGenre(genre_item.mal_id as number, genre_item.name as string, genre_item.type as string, genre_item.url as string));
+          for (let genre_item of genres) {
+            animeGenres.push(
+              new AnimeGenre(
+                genre_item.mal_id as number,
+                genre_item.name as string,
+                genre_item.type as string,
+                genre_item.url as string
+              )
+            );
           }
-          animeList.push(new AnimeInfo(item.mal_id as number, item.title as string, animeImage, animeGenres));
+          animeList.push(
+            new AnimeInfo(
+              item.mal_id as number,
+              item.title as string,
+              animeImage,
+              animeGenres
+            )
+          );
         }
 
         this.searchedAnimeList = animeList;
       },
       error(err) {
-        console.log(`Oh boy, there's an error: ${err}`);
-      },
-      complete() {
-        console.log("My job has done.");
+        console.log(err);
       },
     });
   }
 
   onClickAnime(animeId: number) {
-    this.router.navigate(["anime-info"], {queryParams: {id: animeId}});
+    this.router.navigate(['anime-info'], { queryParams: { id: animeId } });
   }
 }
--- src/app/feature/top-anime/top-anime.component.ts
+++ src/app/feature/top-anime/top-anime.component.ts
@@ -10,57 +10,71 @@ import { AnimeInfoService } from 'src/app/core/services/anime-info.service';
 @Component({
   selector: 'app-top-anime',
   templateUrl: './top-anime.component.html',
-  styleUrls: ['./top-anime.component.scss']
+  styleUrls: ['./top-anime.component.scss'],
 })
 export class TopAnimeComponent implements OnInit {
   topAnimeList: AnimeInfo[] = [];
   constructor(
     private animeService: AnimeInfoService,
     private router: Router,
-    private route: ActivatedRoute,
-    ) { }
+    private route: ActivatedRoute
+  ) {}
 
   async ngOnInit() {
-    let page = Number(this.route.snapshot.paramMap.get('page')); 
-    if (page)
-      await this.getTopAnimeByPage(page);
-    else
-      await this.getTopAnimeList();
-
+    let page = Number(this.route.snapshot.paramMap.get('page'));
+    if (page) await this.getTopAnimeByPage(page);
+    else await this.getTopAnimeList();
   }
 
   async getTopAnimeList() {
     this.animeService.getTopAnimes().subscribe({
       next: (data: any) => {
-        for(let item of data.data) {
+        for (let item of data.data) {
           let image = item.images;
-          let jpg_url: AnimeImageUrl = new AnimeImageUrl(image.jpg.image_url as string, image.jpg.large_image_url as string, image.jpg.small_image_url as string);
-          let webp_url: AnimeImageUrl = new AnimeImageUrl(image.webp.image_url as string, image.webp.large_image_url as string, image.webp.small_image_url as string);
+          let jpg_url: AnimeImageUrl = new AnimeImageUrl(
+            image.jpg.image_url as string,
+            image.jpg.large_image_url as string,
+            image.jpg.small_image_url as string
+          );
+          let webp_url: AnimeImageUrl = new AnimeImageUrl(
+            image.webp.image_url as string,
+            image.webp.large_image_url as string,
+            image.webp.small_image_url as string
+          );
           let animeImage: AnimeImage = new AnimeImage(jpg_url, webp_url);
           let genres = item.genres;
           let animeGenres: AnimeGenre[] = [];
-          for(let genre_item of genres) {
-            animeGenres.push(new AnimeGenre(genre_item.mal_id as number, genre_item.name as string, genre_item.type as string, genre_item.url as string));
+          for (let genre_item of genres) {
+            animeGenres.push(
+              new AnimeGenre(
+                genre_item.mal_id as number,
+                genre_item.name as string,
+                genre_item.type as string,
+                genre_item.url as string
+              )
+            );
           }
-          this.topAnimeList.push(new AnimeInfo(item.mal_id as number, item.title as string, animeImage, animeGenres));
+          this.topAnimeList.push(
+            new AnimeInfo(
+              item.mal_id as number,
+              item.title as string,
+              animeImage,
+              animeGenres
+            )
+          );
         }
       },
       error(err) {
-        console.log(`Oh boy, there's an error: ${err}`);
-      },
-      complete() {
-        console.log("My job has done.");
+        console.log(err);
       },
     });
   }
 
   async getTopAnimeByPage(page: number) {
-    this.animeService.getTopAnimesByPage(page).subscribe((data: any) => {
-      console.log("Got data!");
-    });
+    this.animeService.getTopAnimesByPage(page).subscribe((data: any) => {});
   }
 
   onClickAnime(animeId: number) {
-    this.router.navigate(["anime-info"], {queryParams: {id: animeId}});
+    this.router.navigate(['anime-info'], { queryParams: { id: animeId } });
   }
 }
